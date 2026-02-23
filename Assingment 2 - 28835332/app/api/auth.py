ef5e29e10195484578d31327a9c78f82c9a3242d
AutumnMace <jakdarlington17@gmail.com>
2026-02-23 17:00:53 +0000

    bug testing 3
    Corrected spelling errors

--- Assingment 2 - 28835332/app/api/auth.py
+++ Assingment 2 - 28835332/app/api/auth.py
@@ -12,7 +12,7 @@ router = APIRouter(tags=["auth"])
 @router.post("/login", response_model=Token)
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = crud_user.get_user_by_email(db, form_data.username)
-    if not user or not verify_passowrd(form_data.password, user.password_hash):
+    if not user or not verify_password(form_data.password, user.password_hash):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail = "Incorrect email or password",
